0f66e7fbe7b229cfe8072bb2a9eef3ef57f84727
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2016-05-22 21:38:21 +0700

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+This pen is sort of a mashup I'm playing around with, based on several others created by other users; including the following (subject to change/add/remove as I continue playing with this): 
+- [Header Text] Transparent Action Onomatopoeia by @sudocoda http://codepen.io/sudocoda/pen/xkLie
+- [Background Pattern] Comic Book Half Tone Background by @ashandrien http://codepen.io/ashandrien/pen/rnzsd
+- Comic style panes by @beyondSimple http://codepen.io/beyondSimple/pen/sDibB
+- Comics dialog boxes (webkit) by @fliptheweb http://codepen.io/fliptheweb/pen/BAFjo
+
+// NOT USED YET
+- Comic style masonry divs by @ebrewe http://codepen.io/ebrewe/pen/pehbt
+*/
